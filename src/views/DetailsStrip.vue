76bb154967b19805fec54981b1590f97ad42516e
wasiliev96 <wasiliev.cloud@gmail.com>
2020-08-21 12:11:38 +0300

    agile slider added

--- src/views/DetailsStrip.vue
+++ src/views/DetailsStrip.vue
@@ -1,26 +1,33 @@
 <template>
-  <div class="details-strip">
+  <div class="details-strip h-full">
     <!--    <div class="text-center uneccessary">-->
     <!--      <h1>Details View: {{ this.$route.name }}</h1>-->
     <!--      <h2>id:{{ this.$route.params.id }}</h2>-->
     <!--    </div>-->
-
-    <div class="border-2" v-for="item in this.$store.state.saleItems"
+    <!--    <agile :infinite="false">-->
+    <!--    </agile>-->
+    <div class="border-2 card h-full" v-for="item in this.$store.state.saleItems"
          :id="item.id">
-      <div class="horizontal-strip">
-        <div class="image-card" v-for="image in item.gallery">
+      <agile class="horizontal-strip h-full" centerMode>
+        <div class="image-card h-full" v-for="image in item.gallery">
           <img
               :src="'http://localhost:1337' + image.url" :alt="image.alternativeText">
         </div>
-      </div>
+      </agile>
     </div>
+
   </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from "vue-property-decorator";
+import {VueAgile} from 'vue-agile'
 
-@Component
+@Component({
+  components: {
+    agile: VueAgile
+  }
+})
 export default class extends Vue {
   private scrollYPoint = window.scrollY;
   private currentScreenID: number = 1;
@@ -34,5 +41,12 @@ export default class extends Vue {
 </script>
 
 <style scoped lang="scss">
+.card {
+  width: 100vw;
 
+  .image-card {
+    width:  100vw;
+    height: calc(100vh - 50px);
+  }
+}
 </style>
